08848081bbb6a2314bbb52502cf605ac3a8f970f
Farooque Parvez <farooque.simnainfo@gmail.com>
2023-10-16 15:19:40 +0530

    Initial Commit

---
+++ constants/index.ts
@@ -0,0 +1,96 @@
+export const NavLinks = [
+    { href: '/', key: 'Inspiration', text: 'Inspiration' },
+    { href: '/', key: 'Find Projects', text: 'Find Projects' },
+    { href: '/', key: 'Learn Development', text: 'Learn Development' },
+    { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
+    { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
+  ];
+  
+  export const categoryFilters = [
+    "Frontend",
+    "Backend",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Game Dev",
+    "DevOps",
+    "Data Science",
+    "Machine Learning",
+    "Cybersecurity",
+    "Blockchain",
+    "E-commerce",
+    "Chatbots"
+  ]
+  
+  export const footerLinks = [
+    {
+      title: 'For developers',
+      links: [
+        'Go Pro!',
+        'Explore Development Work',
+        'Development Blog',
+        'Code Podcast',
+        'Open-Source Projects',
+        'Refer a Friend',
+        'Code of Conduct',
+      ],
+    },
+    {
+      title: 'Hire developers',
+      links: [
+        'Post a Job Opening',
+        'Post a Freelance Project',
+        'Search For Developers',
+      ],
+    },
+    {
+      title: 'Brands',
+      links: [
+        'Advertise With Us',
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        'About',
+        'Careers',
+        'Support',
+        'Media Mit',
+        'Testimonials',
+        'API',
+        'Terms of Service',
+        'Privacy Policy',
+        'Cookie Policy',
+      ],
+    },
+    {
+      title: 'Directories',
+      links: [
+        'Development Jobs',
+        'Developers For Hire',
+        'Freelance Developers For Hire',
+        'Tags',
+        'Places',
+      ],
+    },
+    {
+      title: 'Development assets',
+      links: [
+        'Code Marketplace',
+        'GitHub Marketplace',
+        'NPM Registry',
+        'Packagephobia',
+      ],
+    },
+    {
+      title: 'Development Resources',
+      links: [
+        'Freelancing',
+        'Development Hiring',
+        'Development Portfolio',
+        'Development Education',
+        'Creative Process',
+        'Development Industry Trends',
+      ],
+    },
+  ];
